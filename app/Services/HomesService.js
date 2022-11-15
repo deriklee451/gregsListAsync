23af98604eabba4063c49e99b757a2c28e1cce32
Derik Banning <deriklee451@gmail.com>
2022-11-15 16:32:42 -0700

    jhgkjhg

--- app/Services/HomesService.js
+++ app/Services/HomesService.js
@@ -1,5 +1,6 @@
 import { appState } from "../AppState.js"
 import { Home } from "../Models/Home.js"
+import { Pop } from "../Utils/Pop.js"
 
 
 class HomesService {
@@ -8,6 +9,13 @@ class HomesService {
         const res = await axios.get('https://bcw-sandbox.herokuapp.com/api/houses')
         console.log('[Got Homes]', res.data) //NOTE - REALLY import to check got data.
         appState.homes = res.data.map(h => new Home(h))
+    }
+
+    async removeHome(id) {
+        const res = await axios.delete('https://bcw-sandbox.herokuapp.com/api/houses/' + id)
+        console.log('[DELETE CAR]', res.data);
+        Pop.toast(res.data, 'success')
+        appState.homes = appState.homes.filter(h => h.id != id)
     }
 
 }
