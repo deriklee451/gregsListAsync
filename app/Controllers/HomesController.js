eac7e4dbaef4a3476a6df9418edf38ec86b98f46
Derik Banning <deriklee451@gmail.com>
2022-11-15 15:14:39 -0700

    draw commit

--- app/Controllers/HomesController.js
+++ app/Controllers/HomesController.js
@@ -1,6 +1,37 @@
+import { appState } from "../AppState.js";
+import { homesService } from "../Services/HomesService.js";
+import { setHTML } from "../Utils/Writer.js";
 
 
+
+function _drawHomes() {
+    const homes = appState.homes
+    let template = ''
+    homes.forEach(h => template += h.listTemplate)
+    setHTML('listings', template)
+}
 
 export class HomesController {
+    constructor() {
 
+        appState.on('homes', _drawHomes)
+        // this.getHomes()
+    }
+
+    async getHomes() {
+        try {
+            await homesService.getHomes()
+        } catch (error) {
+            // NOTE best catch alerts the user and logs the error
+            Pop.error(error.message)
+            console.error(error)
+        }
+    }
+
+
+
+    showHomes() {
+        _drawHomes()
+
+    }
 }
